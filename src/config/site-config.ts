3b06fcd9f43e1f6f962945a05ea3b16250f5140c
SnakeHacKx <omaralexisxdx2@gmail.com>
2024-10-14 16:26:18 -0500

    blog → post-01.mdx frontmatter does not match collection schema.

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  title: "SnakeHacKx's Blog",
+  description: "Blog oficial de SnakeHacKx --- Compartiendo mis experiencias en desarrollo y diseño web."
+}
---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+import { z } from "astro/zod";
+import { defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image().refine((img) => img.width < 1200, {
+        message: "Image should be shorter than 1200px",
+      }),
+
+      // Relacion
+      author: z.string(),
+
+      // Relacion
+      tags: z.array(z.string()),
+    }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
